6a84005c31828b7e9921123e4095af37e7182c9d
Schwarz, Mario <schmar9@gmx.de>
2025-10-22 16:34:40 +0200

    new options for histogram plotting

--- src/sipm_autocalib/histograms.py
+++ src/sipm_autocalib/histograms.py
@@ -36,6 +36,13 @@ def gen_hist_by_range(data, range, nbins=200):
     n, be = np.histogram(data, range=range, bins=nbins)
     return n, be
 
+def histo_content_at(histo: dict[str, Any], x: np.float64) -> np.float64:
+    """Return content of bin at position x"""
+    if x > histo["be"][-1] or x < histo["be"][0]:
+        raise RuntimeError(f"x={x} out of histogram range [{histo['be'][0]}, {histo['be'][-1]}]")
+    bin_index = np.searchsorted(histo["be"], x) - 1
+    return histo["n"][bin_index]
+
 def plot_all_pe_spectra(energies_dict) -> Figure:
     """Simple helper to plot all raw energy spectra."""
     fig, ax = auto_subplots(len(energies_dict))
@@ -63,13 +70,28 @@ def plot_all_pe_histograms(histos: dict[str, dict[str, Any]], *, gridx = False) 
 
 def plot_all_pe_histograms_and_thresholds(
         histos: dict[str, dict[str, Any]], 
-        thresholds: dict[str, np.float64], *, gridx = False) -> Figure:
-    """Simple helper to draw all available 1-D histograms with labels and thresholds"""
+        thresholds: dict[str, np.float64], *, 
+        gridx = False,
+        logy = True,
+        scaleto: tuple[float, float] | None = None, # x position, multiplier for max y
+        fig_ax: tuple[Figure, np.ndarray] | None = None) -> Figure:
+    """Simple helper to draw all available 1-D histograms with labels and thresholds.
+    
+        Optional arguments:
+        - gridx: If True, adds x-axis grid lines (red if threshold is NaN)
+        - logy: If True, uses logarithmic y-axis scale
+        - scaleto: Tuple of (x_position, multiplier) to scale y-axis limits based on histogram content at x_position
+        - fig_ax: Tuple of (matplotlib Figure, axes array) for plotting on existing figure
+    """
     assert len(histos) <= len(thresholds)
-    fig, ax = auto_subplots(len(histos))
+    fig, ax = fig_ax if fig_ax is not None else auto_subplots(len(histos))
     ax = ax.ravel()
     for i, (name, histo) in enumerate(histos.items()):
-        ax[i].set_yscale('log')
+        if logy:
+            ax[i].set_yscale('log')
+        if scaleto is not None:
+            ref = histo_content_at(histo, scaleto[0])
+            ax[i].set_ylim(0.9, ref * scaleto[1])
         ax[i].stairs(histo["n"], histo["be"])
         ax[i].set_title(name)
         if not np.isnan(thresholds[name]):
